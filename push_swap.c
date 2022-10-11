e7748c6b82d94ab9791117c62b50918a356b649e
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-10-11 22:00:21 +0900

    feat : parse order method

--- push_swap.c
+++ push_swap.c
@@ -72,29 +72,53 @@ t_node	*ft_pop_stack_node(t_stack *stack)
 		temp = stack->head;
 		stack->head = stack->head->next;
 	}
-
 	return (temp);
+}
+
+void	ft_parse_data(t_stack *a, int ac, char **av)
+{
+	int		i;
+	int		j;
+	t_node	*temp;
+	t_node	*current;
+
+	i = 0;
+	while (++i < ac)
+		ft_push_stack_data(a, ft_atoi(*(av + i)));
+	current = a->head;
+	i = -1;
+	while (++i < ac - 1)
+	{
+		temp = current->next;
+		j = -1;
+		while (++j < ac - 2)
+		{
+			if (temp->data == current->data)
+				exit(6);
+			if (temp->data < current->data)
+				++(current->order);
+			temp = temp->next;
+		}
+		current = current->next;
+	}
 }
 
 int	main(int ac, char **av)
 {
-	int		i;
 	t_stack	a;
 	t_stack	b;
 
 	ft_init_stack(&a);
 	ft_init_stack(&b);
-	i = 0;
-	while (++i < ac)
-	{
-		ft_push_stack_data(&a, ft_atoi(*(av + i)));
-	}
 
+	ft_parse_data(&a, ac, av);
+
+	// debug
 	t_node	*temp;
 	temp = a.head;
 	for (int k = 0; k < ac - 1; ++k)
 	{
-		printf("%d\n", temp->data);
+		printf("%d\t%d\n", temp->data, temp->order);
 		temp = temp->next;
 	}
 	printf("\nstack size : %d", a.size);
